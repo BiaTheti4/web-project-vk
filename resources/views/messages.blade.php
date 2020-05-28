5a6ff71aa23a25ba8745076c3a30633d26f028b2
BiaTheTi4 <daniellikhachev@gmail.com>
2020-05-28 17:59:42 +0800

    add other files

--- resources/views/messages.blade.php
+++ resources/views/messages.blade.php
@@ -12,7 +12,7 @@
         <div class="d3">
             <form>
                 <input type="text" placeholder="Искать здесь...">
-                <button type="submit"><img src="https://image.flaticon.com/icons/svg/2089/2089805.svg" class="search_main" type="submit"></button>
+                <button type="submit"><img src="media\search.svg" class="search_main" type="submit"></button>
             </form>
         </div>
         <div class="header_icons">
